a258c7a94e627189f71e6a6826994165e4cbadb6
takidilmi <angeldilmi@gmail.com>
2023-08-30 00:06:50 +0200

    Completed assignment

--- main.js
+++ main.js
@@ -3,7 +3,35 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", () => {
+  const errorModal = document.getElementById("modal");
+  const hearts = document.querySelectorAll(".like-glyph");
 
+  // Add .hidden class to error modal initially
+  errorModal.classList.add("hidden");
+
+  hearts.forEach(heart => {
+    heart.addEventListener("click", () => {
+      mimicServerCall()
+        .then(() => {
+          // Toggle the activated-heart class to make heart appear red
+          heart.classList.toggle("activated-heart");
+          // Change heart to full heart
+          heart.textContent = FULL_HEART;
+        })
+        .catch(error => {
+          // Display error modal and message on failure
+          errorModal.classList.remove("hidden");
+          errorModal.textContent = error;
+          
+          setTimeout(() => {
+            // Hide the modal after 3 seconds
+            errorModal.classList.add("hidden");
+          }, 3000);
+        });
+    });
+  });
+});
 
 
 
